13b91ff0400caf6c0f7ba421f96c4f220eaf5109
Vinicius Rodrigues <viniciusr.camargo@gmail.com>
2023-04-11 17:59:43 -0300

    create delete button

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,16 +1,18 @@
 <x-layout title="Séries">
     <a class="btn btn-dark mb-2" href="{{'series.store'}}">Adicionar</a>
+    <a href="#">teste</a>
     <ul class="list-group" >
         @foreach ($series as $serie)
-            <li class="list-group-item d-flex justify-content-between align-items-center">{{$serie->nome}}
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                {{$serie->nome}}
+
                 <form action="{{ route('series.destroy', $serie->id)}}" method="post">
-                    <button class="btn btn-danger btn-sm">X</button>
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
                 </form>
             </li>
-
-
         @endforeach
     </ul>
-
 </x-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,10 @@ Route::get('/', function () {
     return redirect('/series');
 });
 
-Route::resource('/series', SeriesController::class)->only(
-    ['index', 'create', 'store']);
+/*Route::resource('/series', SeriesController::class)->only(
+    ['index', 'create', 'store']);*/
+
+Route::resource('/series', SeriesController::class);
 
 Route::post('/series/destroy/{$id}', [SeriesController::class, 'destroy'])->name('series.destroy');
 
