a9c31d5c2677f8fdeb49a66567a6e7df43df541b
Vinicius Rodrigues <viniciusr.camargo@gmail.com>
2023-04-12 16:54:49 -0300

    Creating Flash messages

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         //$series = Serie::all();
         $series = Serie::query()->orderBy('nome')->get();
-
-        return view('series.index')->with('series', $series);
+        $mensagemSucesso = $request->session()->get('mensagem.sucesso');
+        //dd($mensagemSucesso);
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create(){
@@ -24,7 +25,8 @@ class SeriesController extends Controller
         //Serie::create($request->only('nome')); busca somente os campos da requisição que for informado
         //Serie::create($request->except('_token')); busca tudo exceto o informado
         Serie::create($request->all());
-
+        $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso');
+        //dd($request->session());
         return to_route('series.index');
     }
 
@@ -32,6 +34,7 @@ class SeriesController extends Controller
     {
         //dd($request->route());
         Serie::destroy($request->series);
+        $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso');
 
         return to_route('series.index');
     }
